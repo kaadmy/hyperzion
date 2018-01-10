dd0a9ac9e4e810f3fc3996cb02e6245c096e41df
KaadmY <kaadmy@gmail.com>
2018-01-09 18:14:55 -0800

    Fix error if running CMake with a downloaded snapshot

--- src/revision.cmake
+++ src/revision.cmake
@@ -8,11 +8,11 @@ if (GIT_FOUND)
     RESULT_VARIABLE GIT_REVISION_VALID
     OUTPUT_VARIABLE GIT_REVISION)
 
-  string(REPLACE "\n" "" GIT_REVISION ${GIT_REVISION})
-
   if (NOT ${GIT_REVISION_VALID} EQUAL 0)
     message(WARNING "${GIT_REVISION_COMMAND} failed, build will not have a revision.")
     set(GIT_REVISION "unknown")
+  else()
+    string(REPLACE "\n" "" GIT_REVISION ${GIT_REVISION})
   endif()
 else()
   message(WARNING "Git not found, build will not have a revision")
